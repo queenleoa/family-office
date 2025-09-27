53f6f7c231f29c0fcab94dcfd6c73dc0f42c8114
queenleoa <adrija11235@gmail.com>
2025-09-27 14:21:08 +0000

    reset and let's ggo

--- src/tokens/MockPYUSD.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-import {ERC20} from "solmate/src/tokens/ERC20.sol";
-
-contract MockPYUSD is ERC20("PayPal USD", "PYUSD", 6) {
-    function mint(address to, uint256 amt) external { _mint(to, amt); }
-}
--- src/tokens/MockWETH.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-import {ERC20} from "solmate/src/tokens/ERC20.sol";
-
-contract MockWETH is ERC20("Wrapped Ether", "WETH", 18) {
-    function mint(address to, uint256 amt) external { _mint(to, amt); }
-}
